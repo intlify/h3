8587440eea13a5e5a64af499456fbb0fa06c80e1
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-16 02:17:35 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import type { H3Event } from 'h3'
 /**
  * get accpet languages
  * @description parse `accept-language` header string
- * @param event The H3 event
- * @returns The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
+ * @param {H3Event} event The H3 event
+ * @returns {Array<string>} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
  */
 export function getAcceptLanguages(event: H3Event): string[] {
   const headers = getHeaders(event)
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 /**
  * parse `accept-language` header string
  * @param {string} value The accept-language header string
- * @returns {string[]} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
+ * @returns {Array<string>} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
  */
 export function parseAcceptLanguage(value: string): string[] {
   return value.split(',').map((tag) => tag.split(';')[0]).filter((tag) =>
