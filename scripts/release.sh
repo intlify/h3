dd7235c36596472dba9ec269ffea1781488571da
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-17 00:21:30 +0900

    chore: setup release workflow

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xe
+
+# Check edge release
+if [[ ! -z ${EDGE_RELEASE} ]] ; then
+  bunx tsx ./scripts/bump-edge
+fi
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release packages
+echo "Publishing"
+npm publish
