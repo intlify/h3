97133f2f915f7e4ac0c50e1d7839da9a6f5250ef
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-16 15:37:24 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import type { H3Event } from 'h3'
  *
  * @description parse `accept-language` header string
  *
- * @param {H3Event} event The H3 event
+ * @param {H3Event} event The {@link H3Event | H3} event
  *
  * @returns {Array<string>} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
  */
@@ -21,10 +21,10 @@ export function getAcceptLanguages(event: H3Event): string[] {
 /**
  * get locale
  *
- * @param {H3Event} event The H3 event
+ * @param {H3Event} event The {@link H3Event | H3} event
  * @param {string} lang The default language tag, default is `en-US`. You must specify the language tag with the {@link https://datatracker.ietf.org/doc/html/rfc4646#section-2.1 | BCP 47 syntax}.
  *
- * @throws {RangeError} Throws a `RangeError` if `lang` option or `accpet-languages` are not a well-formed BCP 47 language tag.
+ * @throws {RangeError} Throws a {@link RangeError} if `lang` option or `accpet-languages` are not a well-formed BCP 47 language tag.
  *
  * @returns {Intl.Locale} The locale that resolved from `accept-language` header string, first language tag is used. if `*` (any language) or empty string is detected, return `en-US`.
  */
@@ -36,11 +36,11 @@ export function getLocale(event: H3Event, lang = 'en-US'): Intl.Locale {
 /**
  * get locale from cookie
  *
- * @param {H3Event} event The H3 event
+ * @param {H3Event} event The {@link H3Event | H3} event
  * @param {string} options.lang The default language tag, default is `en-US`. You must specify the language tag with the {@link https://datatracker.ietf.org/doc/html/rfc4646#section-2.1 | BCP 47 syntax}.
  * @param {string} options.name The cookie name, default is `i18n_locale`
  *
- * @throws {RangeError} Throws a `RangeError` if `lang` option or cookie name value are not a well-formed BCP 47 language tag.
+ * @throws {RangeError} Throws a {@link RangeError} if `lang` option or cookie name value are not a well-formed BCP 47 language tag.
  *
  * @returns The locale that resolved from cookie
  */
