4dc7b0e05279b365c43199ad4e58801aa69fcaf0
kazuya kawaguchi <kawakazu80@gmail.com>
2023-09-16 15:23:09 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ import type { H3Event } from 'h3'
 
 /**
  * get accpet languages
+ *
  * @description parse `accept-language` header string
+ *
  * @param {H3Event} event The H3 event
+ *
  * @returns {Array<string>} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
  */
 export function getAcceptLanguages(event: H3Event): string[] {
@@ -17,8 +20,10 @@ export function getAcceptLanguages(event: H3Event): string[] {
 
 /**
  * get locale
+ *
  * @param {H3Event} event The H3 event
  * @param {string} lang The default language tag, default is `en-US`. You must specify the language tag with the {@link https://datatracker.ietf.org/doc/html/rfc4646#section-2.1 | BCP 47 syntax}.
+ *
  * @returns {Intl.Locale} The locale that resolved from `accept-language` header string, first language tag is used. if `*` (any language) or empty string is detected, return `en-US`.
  */
 export function getLocale(event: H3Event, lang = 'en-US'): Intl.Locale {
@@ -32,6 +37,7 @@ export function getLocale(event: H3Event, lang = 'en-US'): Intl.Locale {
  * @param {H3Event} event The H3 event
  * @param {string} options.lang The default language tag, default is `en-US`. You must specify the language tag with the {@link https://datatracker.ietf.org/doc/html/rfc4646#section-2.1 | BCP 47 syntax}.
  * @param {string} options.name The cookie name, default is `i18n_locale`
+ *
  * @returns The locale that resolved from cookie
  */
 export function getCookieLocale(
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 /**
  * parse `accept-language` header string
+ *
  * @param {string} value The accept-language header string
+ *
  * @returns {Array<string>} The array of language tags, if `*` (any language) or empty string is detected, return an empty array.
  */
 export function parseAcceptLanguage(value: string): string[] {
